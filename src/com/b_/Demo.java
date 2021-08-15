117ff4b0d5efb4e256ff2a4a5b720be04b6b6ded
1540821472 <1540821472@qq.com>
2021-08-15 08:35:27 +0800

    java基础学习第一次提交

---
+++ src/com/b_/Demo.java
@@ -0,0 +1,15 @@
+package com.b_;
+
+
+import java.sql.SQLOutput;
+import java.util.Random;
+
+/**
+ * @author Administrator
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println(random.nextInt()*(11));
+    }
+}
---
+++ src/com/b_/Test.java
@@ -0,0 +1,9 @@
+package com.b_;
+
+/**
+ * @auther sunjiangdong
+ * @date 2021/8/15
+ */
+public class Test {
+
+}
